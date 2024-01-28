599804fecb7c2685cc5c27c36a981b443567e1c3
ctorresm2 <ctorresm0104@gmail.com>
2024-01-28 01:10:02 -0500

    Se agrega el Ejemplo

---
+++ Proyecto_ROL/Data/DA_Logica.cs
@@ -0,0 +1,25 @@
+﻿using Proyecto_ROL.Models;
+
+namespace Proyecto_ROL.Data
+{
+    public class DA_Logica
+    {
+        public List<Usuario> ListaUsuarios() {
+
+            return new List<Usuario>
+            {
+                new Usuario{Nombre="jose",Correo="administrado@gmail.com",Clave="123", Roles= new string[]{"Administrador"} },
+                new Usuario{Nombre="maria",Correo="supervisor@gmail.com",Clave="123", Roles= new string[]{"Supervisor"} },
+                new Usuario{Nombre="juan",Correo="empleado@gmail.com",Clave="123", Roles= new string[]{"Empleado"} },
+                new Usuario{Nombre="oscar",Correo="superempleado@gmail.com",Clave="123", Roles= new string[]{"Supervisor ","Empleado"} }
+
+            };
+        }
+
+        public Usuario ValidarUsuario (string _correo, string _clave) { 
+            return ListaUsuarios().Where(item => item.Correo == _correo && item.Clave == _clave).FirstOrDefault();
+        
+        }
+
+    }
+}
---
+++ Proyecto_ROL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_ROL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
